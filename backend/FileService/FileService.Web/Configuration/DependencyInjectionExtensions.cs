51f266fffc2bc672c80032b8966829a6cfd10106
Yunus Ibragimov <yunus.ibragimov49@gmail.com>
2025-11-20 20:48:48 +0300

    Базовая настройка FileService

---
+++ backend/FileService/FileService.Web/Configuration/DependencyInjectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using FileService.Core;
+using Framework.Endpoints;
+using Framework.Logging;
+using Framework.Swagger;
+
+namespace FileService.Web.Configuration;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddSerilogLogging(configuration, "FileService")
+            .AddOpenApiSpec("FileService", "v1")
+            .AddEndpoints(typeof(DependencyInjectionCoreExtensions).Assembly);
+
+        services
+            .AddCore(configuration);
+
+        return services;
+    }
+}
