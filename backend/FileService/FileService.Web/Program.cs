5697cdb166c0d73df268884e86badb39b82726aa
Yunus Ibragimov <yunus.ibragimov49@gmail.com>
2025-11-25 13:58:18 +0300

    Автозапуск миграций при запуске в dev режиме

--- backend/FileService/FileService.Web/Program.cs
+++ backend/FileService/FileService.Web/Program.cs
@@ -1,5 +1,7 @@
 using System.Globalization;
+using FileService.Infrastructure.Postgres;
 using FileService.Web.Configuration;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -13,9 +15,9 @@ try
 
     WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-    string environment = builder.Environment.EnvironmentName;
+    var environment = builder.Environment;
 
-    builder.Configuration.AddJsonFile($"appsettings.{environment}.json", true, true);
+    builder.Configuration.AddJsonFile($"appsettings.{environment.EnvironmentName}.json", true, true);
 
     builder.Configuration.AddEnvironmentVariables();
 
@@ -25,6 +27,16 @@ try
 
     app.Configure();
 
+    bool autoMigrate = builder.Configuration.GetSection("Database").GetValue<bool>("AutoMigrate");
+    if (autoMigrate && environment.IsDevelopment())
+    {
+         using (var scope = app.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<FileServiceDbContext>();
+            context.Database.Migrate();
+        }
+    }
+    
     app.Run();
 }
 catch (Exception ex)
