bff5b8bfcb3b881c5d3c59eb05c8e140fa889164
Yunus Ibragimov <yunus.ibragimov49@gmail.com>
2025-11-27 18:38:13 +0300

    Новый конфиг S3

--- backend/FileService/FileService.Infrastructure.S3/DependencyInjectionS3Extensions.cs
+++ backend/FileService/FileService.Infrastructure.S3/DependencyInjectionS3Extensions.cs
@@ -1,8 +1,10 @@
 ﻿using Amazon.Extensions.NETCore.Setup;
 using Amazon.Runtime;
 using Amazon.S3;
+using FileService.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace FileService.Infrastructure.S3;
 
@@ -12,20 +14,21 @@ public static class DependencyInjectionS3Extensions
     {
         services.Configure<S3Options>(configuration.GetSection(nameof(S3Options)));
 
-        S3Options s3Options = configuration.GetSection(nameof(S3Options)).Get<S3Options>()
-                        ?? throw new ApplicationException("Missing s3 configuration section: S3Options.");
-        
-        var options = new AWSOptions { 
-            DefaultClientConfig =
-            {
+        services.AddScoped<IS3Provider, S3Provider>();
+
+        services.AddSingleton<IAmazonS3>(sp =>
+        {
+            S3Options s3Options = sp.GetRequiredService<IOptions<S3Options>>().Value;
+            
+            var config = new AmazonS3Config { 
                 ServiceURL = configuration.GetConnectionString(s3Options.Endpoint),
                 UseHttp = !s3Options.WithSsl,
-            },
-            Credentials = new BasicAWSCredentials(s3Options.AccessKey, s3Options.SecretKey),
-        };
-
-        services.AddAWSService<IAmazonS3>(options);
-
+                ForcePathStyle = true,
+            };
+            
+            return new AmazonS3Client(s3Options.AccessKey, s3Options.SecretKey, config);
+        });
+        
         return services;
     }
 }
