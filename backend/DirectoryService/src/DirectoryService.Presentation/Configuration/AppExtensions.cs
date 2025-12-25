90ca491b206d5402eb054b09f59a6a3c179ac15b
Yunus Ibragimov <yunus.ibragimov49@gmail.com>
2025-12-25 17:07:26 +0300

    Небольшой рефакторинг Program.cs и конфигураций

---
+++ backend/DirectoryService/src/DirectoryService.Presentation/Configuration/AppExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Framework.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace DirectoryService.Presentation.Configuration;
+
+public static class AppExtensions
+{
+    public static IApplicationBuilder Configure(this WebApplication app)
+    {
+        app.UseCors(corsPolicyBuilder =>
+        {
+            corsPolicyBuilder.WithOrigins(
+                    app.Configuration.GetValue<string>("CorsAllowedOrigins:React", "http://localhost:3000"))
+                .AllowCredentials()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+
+        app.UseExceptionMiddleware();
+
+        app.UseSerilogRequestLogging();
+
+        app.MapOpenApi();
+
+        app.UseSwagger();
+        app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "DirectoryService V1"));
+
+        app.MapControllers();
+
+        return app;
+    }
+}
